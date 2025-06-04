475dc60bfbcfe0cc4c3d6664c2e61850f0001efd
abhilashtk0922 <abhilashtk2004@gmail.com>
2025-06-04 21:47:07 +0530

    Initial commit

--- std/src/main/java/com/example/App.java
+++ std/src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class App {
+    public static String greet() {
+        return "Hello, Maven!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World from a proper main class!");
+        System.out.println(greet());
     }
 }
